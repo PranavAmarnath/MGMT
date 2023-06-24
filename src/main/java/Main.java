ca78e23bf38b7a576b8b27058635f751658efa04
Pranav Amarnath <zonepranav@gmail.com>
2023-06-23 17:18:51 -0700

    replaced absolute path with relative path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,22 +6,37 @@ import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 import javax.swing.*;
 import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Main {
 
     private static Model model;
+    private static File file;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         FlatLightLaf.setup();
         SwingUtilities.invokeLater(View::new);
+        file = getFileFromResource("data.csv");
+    }
+
+    private static File getFileFromResource(String fileName) throws URISyntaxException {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
     }
 
     public static void createModel(JTable table, boolean refresh) {
-        model = new Model(new File("C:\\Users\\zonep\\IdeaProjects\\MGMT\\src\\main\\resources\\data.csv"), table, refresh);
+        model = new Model(file, table, refresh);
     }
 
     public static void saveModel(JTable table) {
-        Model.save("C:\\Users\\zonep\\IdeaProjects\\MGMT\\src\\main\\resources\\data.csv", table);
+        //Model.save("C:\\Users\\zonep\\IdeaProjects\\MGMT\\src\\main\\resources\\data.csv", table);
+        Model.save(file.getPath(), table);
     }
 
     public static Model getModel() {
