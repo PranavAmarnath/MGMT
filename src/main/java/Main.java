ca8e2020bc7f9d5ae8d82c7f704a0b6e6b35c1fb
Pranav Amarnath <zonepranav@gmail.com>
2023-06-24 21:28:10 -0700

    try try again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,9 +37,6 @@ public class Main {
                     desktop.setPreferencesHandler(e -> {
                         JOptionPane.showOptionDialog(View.getFrame(), "Preferences", "MGMT Preferences", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[]{}, null);
                     });
-                    desktop.setQuitHandler((e,r) -> {
-                        View.getFrame().dispose();
-                    });
                 });
             } catch (Exception e) { e.printStackTrace(); }
         }
